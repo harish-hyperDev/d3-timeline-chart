0414ad1fd71a188e0ea6d51cac7a0cd4f30ac49a
harish-hyperDev <coolprogrammer.777@gmail.com>
2023-01-25 19:05:35 +0530

    added tooltip and proceeding to experimental features

--- src/basic.js
+++ src/basic.js
@@ -2,16 +2,16 @@ var margin = { top: 20, right: 20, bottom: 30, left: 50 },
     width = 960,
     height = 500 - margin.top - margin.bottom;
 
-// Step 2: Define the data
+
 var data = [
-    { date: "2000-01-01", value: 50 },
-    { date: "2001-01-01", value: 75 },
-    { date: "2002-01-01", value: 100 },
-    { date: "2003-01-01", value: 125 },
-    { date: "2004-01-01", value: 150 }
+    { date: "2000-01-01", value: 50, name: "Michael" },
+    { date: "2001-01-01", value: 75, name: "Johnson" },
+    { date: "2002-01-01", value: 100, name: "David" },
+    { date: "2003-01-01", value: 125, name: "Patrick" },
+    { date: "2004-01-01", value: 150, name: "James" }
 ];
 
-// Step 3: Create scales and axes
+
 var x = d3.scaleTime()
     .domain(d3.extent(data, function (d) { return d.value; }))
     .range([0, width]);
@@ -23,7 +23,18 @@ var y = d3.scaleLinear()
 var xAxis = d3.axisBottom(x)
 // var yAxis = d3.axisLeft(y);
 
-// Step 4: Append SVG elements and bind data
+var tooltip = d3.select("body")
+                .append("div")
+                .attr('class', 'tooltip')
+                .style("position", "absolute")
+                .style("z-index", "10")
+                .style("visibility", "hidden")
+                .style("background", "#fff")
+                .style("padding", "5px")
+                .style("border-radius", "7px")
+                .text("");
+
+
 var svg = d3.select("#timeline-chart").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
@@ -37,7 +48,26 @@ svg.selectAll(".dot")
     .attr("class", "dot")
     .attr("cx", function (d) { return x(d.value); })
     .attr("cy", function () { return height; })
-    .attr("r", 5);
+    .attr("r", 5)
+    .on("mouseover", (d) => { 
+        svg.selectAll(".dot").style("cursor", "pointer");
+        svg.select("path").style("cursor", "pointer");
+        tooltip.text(d.name); 
+        return tooltip.style("visibility", "visible");
+    })
+
+.on("mousemove", () => { 
+        
+        return tooltip.style("top", (d3.event.pageY-30)+"px")
+                      .style("left",(d3.event.pageX-15)+"px")
+                      
+})
+
+.on("mouseout", () => { 
+                    svg.selectAll(".dot").style("cursor", "default");
+                    svg.select("path").style("cursor", "default");
+                    return tooltip.style("visibility", "hidden") 
+                });
 
 svg.append("g")
     .attr("class", "x axis")
