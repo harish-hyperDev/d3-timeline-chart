c07da16607f11becb5b4df295907df38a27cbaff
harish-hyperDev <coolprogrammer.777@gmail.com>
2023-01-25 19:10:27 +0530

    fit chart in center

--- src/basic.js
+++ src/basic.js
@@ -1,6 +1,6 @@
 var margin = { top: 20, right: 20, bottom: 30, left: 50 },
     width = 960,
-    height = 500 - margin.top - margin.bottom;
+    height = 350 - margin.top - margin.bottom;
 
 
 var data = [
